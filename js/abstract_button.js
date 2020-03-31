638b7c4b214be0fdd602be5ac5c7f69f97f67d67
csbhr <baihaoran@njust.edu.cn>
2020-03-31 16:06:39 +0800

    apply Jekyll

--- js/abstract_button.js
+++
@@ -1,8 +0,0 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        this.classList.toggle("active");
-        this.parentNode.nextElementSibling.classList.toggle("show");
-  }
-}
